6a3d499e4def976b32b2adae8b75c2da62f336fb
Anže Toman <78642908+tomananze1997@users.noreply.github.com>
2026-04-11 12:22:22 +0200

    feat/images
    -changed images to v and c class
    -edited fleet section for two newly added vehicles

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,8 +1,8 @@
 import { IEnvironment } from '@interfaces';
 
 export const environment: IEnvironment = {
-  REDIRECT_LINK: '',
-  EMAIL_SERVICE_ID: '',
-  EMAIL_TEMPLATE_ID: '',
-  EMAIL_PUBLIC_KEY: ''
+  REDIRECT_LINK: 'http://localhost:4200',
+  EMAIL_SERVICE_ID: 'service_w6d9aly',
+  EMAIL_TEMPLATE_ID: 'template_co3og1e',
+  EMAIL_PUBLIC_KEY: '9KVzWm-hzoCLcqJng'
 };
